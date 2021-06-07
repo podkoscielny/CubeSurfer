4a605f8083e23077a8f9ad1d9be04e88e1aefa89
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-06-07 12:07:20 +0200

    Add reference to the main camera in MenuCubeController.cs

--- CubeSurfer/Assets/Scripts/MenuCubeController.cs
+++ CubeSurfer/Assets/Scripts/MenuCubeController.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class MenuCubeController : MonoBehaviour
 {
+    private Camera mainCamera;
+
+    void Start() => mainCamera = Camera.main;
+
     void Update() => FollowMousePosition();
 
     void FollowMousePosition()
     {
         Vector3 mousePosition = Input.mousePosition;
-        Vector3 mousePositionWorld = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.y));
+        Vector3 mousePositionWorld = mainCamera.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, transform.position.y));
         transform.LookAt(mousePositionWorld);
     }
 }
