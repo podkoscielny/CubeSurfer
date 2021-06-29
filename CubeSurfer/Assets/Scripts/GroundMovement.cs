09c27422696ea202697542a283c05e00fe07632a
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-06-29 17:08:59 +0200

    Change ground movement speed on level up

---
+++ CubeSurfer/Assets/Scripts/GroundMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundMovement : MonoBehaviour
+{
+    private GameManager _gameManager;
+    private ParticleSystem _groundParticles;
+
+    #region EventSubscribers
+    void OnEnable()
+    {
+        StartGameCamera.OnGameStart += StartGroundMovement;
+        MoveObstacleToPool.OnLevelup += SpeedUpGroundMovement;
+    }
+    void OnDisable()
+    {
+        StartGameCamera.OnGameStart -= StartGroundMovement;
+        MoveObstacleToPool.OnLevelup -= SpeedUpGroundMovement;
+    }
+    #endregion
+
+    void Start()
+    {
+        _gameManager = GameManager.Instance;
+        _groundParticles = GetComponent<ParticleSystem>();
+    }
+
+    void StartGroundMovement() => _groundParticles.Play();
+
+    void SpeedUpGroundMovement()
+    {
+        ParticleSystem.VelocityOverLifetimeModule velocity = _groundParticles.velocityOverLifetime;
+
+        AnimationCurve curve = new AnimationCurve();
+        curve.AddKey(0.0f, 1.0f);
+        curve.AddKey(1.0f, 0.0f);
+        float speed = _gameManager.CurrentSpeed * 1.5f;
+        velocity.x = new ParticleSystem.MinMaxCurve(speed, curve);
+    }
+}
