d6fb21d50738664b39d41c1b55869b24c1e641b6
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2021-07-14 15:42:26 +0200

    Move switching lights logic to separate script

--- CubeSurfer/Assets/Scripts/EnvironmentController.cs
+++ CubeSurfer/Assets/Scripts/EnvironmentController.cs
@@ -10,35 +10,20 @@ public class EnvironmentController : MonoBehaviour
     [SerializeField] GameObject boxVolume;
     [SerializeField] GameObject dummySun;
     [SerializeField] GameObject flyingLamps;
+    [SerializeField] GameObject lights;
 
     [Header("Cameras")]
     [SerializeField] Camera mainCamera;
-
-    [Header("Lights")]
-    [SerializeField] Material lightBulbsMaterial;
-    [SerializeField] Material ledStripsMaterial;
-    [SerializeField] Light[] lights;
-
-    [Header("Audio")]
-    [SerializeField] AudioClip switchOnAudio;
-    [SerializeField] AudioClip switchOffAudio;
 
     private Theme _currentTheme;
     private Color _ambientLight;
     private GameManager _gameManager;
     private Animator _environmentAnimator;
-    private AudioSource _environmentAudio;
     private Material _cloudsMaterial;
     private bool _areLightsTurnedOn = false;
-    private Color _turnedLightsOffColor = new Color(r: 0, g: 0, b: 0, a: 0);
-    private Color _lightBulbsEmissionColor = new Color(r: 1.304f, g: 1.270f, b: 1.086f, a: 1f);
-    private Color _ledStripsEmissionColor = new Color(r: 1.662f, g: 1.662f, b: 1.662f, a: 1f);
     private const float TURN_LIGHTS_AT_INTENSITY = 0.55f;
-    private const float LIGHTS_INTENSITY = 62f;
     private const float BASE_CLOUDS_INTENSITY = 1.46f;
     private const float BASE_FRESNEL_OPACITY = 1f;
-    private const float SWITCH_ON_PITCH = 1.8f;
-    private const float SWITCH_OFF_PITCH = 0.8f;
 
     #region Event Subscribers
     private void OnEnable()
@@ -60,11 +45,7 @@ public class EnvironmentController : MonoBehaviour
         _currentTheme = _gameManager.Theme;
         _ambientLight = RenderSettings.ambientLight;
         _environmentAnimator = GetComponent<Animator>();
-        _environmentAudio = GetComponent<AudioSource>();
         _cloudsMaterial = GameObject.FindGameObjectWithTag("Clouds").GetComponent<Renderer>().material;
-
-        lightBulbsMaterial.SetColor("_EmissionColor", _turnedLightsOffColor);
-        ledStripsMaterial.SetColor("_EmissionColor", _turnedLightsOffColor);
     }
 
     void Update()
@@ -106,21 +87,5 @@ public class EnvironmentController : MonoBehaviour
 
     void EnableFlyingLamps() => flyingLamps.SetActive(true);
 
-    void TurnLightsOn() => SwitchLights(_lightBulbsEmissionColor, _ledStripsEmissionColor, LIGHTS_INTENSITY, SWITCH_ON_PITCH, switchOnAudio);
-
-    void TurnLightsOff() => SwitchLights(_turnedLightsOffColor, _turnedLightsOffColor, 0, SWITCH_OFF_PITCH, switchOffAudio);
-
-    void SwitchLights(Color lampsColor, Color ledsColor, float lightsIntensity, float pitch, AudioClip switchingAudio)
-    {
-        lightBulbsMaterial.SetColor("_EmissionColor", lampsColor);
-        ledStripsMaterial.SetColor("_EmissionColor", ledsColor);
-
-        for (int i = 0; i < lights.Length; i++)
-        {
-            lights[i].intensity = lightsIntensity;
-        }
-
-        _environmentAudio.pitch = pitch;
-        _environmentAudio.PlayOneShot(switchingAudio);
-    }
+    void SwitchLights() => lights.SetActive(_areLightsTurnedOn); //Invoke on Animation Event
 }
---
+++ CubeSurfer/Assets/Scripts/SwitchLights.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchLights : MonoBehaviour
+{
+    [Header("Light Materials")]
+    [SerializeField] Material ledsMaterial;
+    [SerializeField] Material lightsMaterial;
+
+    [Header("Audio")]
+    [SerializeField] AudioClip switchOnAudio;
+    [SerializeField] AudioClip switchOffAudio;
+
+    private AudioSource _lightsAudio;
+    private Color _turnedLightsOffColor = new Color(r: 0, g: 0, b: 0, a: 0);
+    private Color _lightBulbsEmissionColor = new Color(r: 1.304f, g: 1.270f, b: 1.086f, a: 1f);
+    private Color _ledStripsEmissionColor = new Color(r: 1.662f, g: 1.662f, b: 1.662f, a: 1f);
+
+    private const float SWITCH_ON_PITCH = 1.8f;
+    private const float SWITCH_OFF_PITCH = 0.8f;
+
+    private void Awake() => 
+        _lightsAudio = GameObject.FindGameObjectWithTag("FlyingLamps").GetComponent<AudioSource>();
+
+    void OnEnable() => 
+        SwitchLamps(_lightBulbsEmissionColor, _ledStripsEmissionColor, SWITCH_ON_PITCH, switchOnAudio);
+
+    void OnDisable() => 
+        SwitchLamps(_turnedLightsOffColor, _turnedLightsOffColor, SWITCH_OFF_PITCH, switchOffAudio);
+
+    void SwitchLamps(Color lampsColor, Color ledsColor, float pitch, AudioClip switchingAudio)
+    {
+        lightsMaterial.SetColor("_EmissionColor", lampsColor);
+        ledsMaterial.SetColor("_EmissionColor", ledsColor);
+
+        _lightsAudio.pitch = pitch;
+        _lightsAudio.PlayOneShot(switchingAudio);
+    }
+}
